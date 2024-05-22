56210bb5a18754badec61a6c1f0e4b5fb229b9a7
Irina <149883699+irinakravtsova@users.noreply.github.com>
2024-05-22 19:45:46 +0300

    Delete constants.js

--- constants.js
+++
@@ -1,6 +0,0 @@
-export const ECOMMERCE_ITEMS_STORAGE_KEY = 'items';
-export const ECOMMERCE_ORDERS_STORAGE_KEY = 'orders';
-export const ITEM_LOCALSTORAGE_KEY = 'bag';
-export const CURRENSY = '$';
-
-
